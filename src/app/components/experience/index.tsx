1afdb2614f4bdc0bda3d13b5bbc1198870e188e1
FehRoque <feliperamosroque@gmail.com>
2023-10-03 14:41:04 -0400

    About me and Experience better details

--- src/app/components/experience/index.tsx
+++ src/app/components/experience/index.tsx
@@ -1,30 +1,44 @@
 import SectionTitle from "../section-title";
 import Skill from "../skill";
 
-import "./styles.css"
-
+import "./styles.css";
 
 export default function Experience() {
-    return (
-        <div className="experience">
-            <SectionTitle text="Experience"/>
-            <p>{"2-3 years working as a Software Developer, I'm familiar with Web Development Full-Stack, Python Programming and RPA Solutions."}</p>
-            <p>{"I'm currently looking for new opportunities to help teams as a"} <em>{"Full-Stack Web Developer, Mobile Developer, DevOps, Python Developer and/or RPA Developer"}</em>.</p>
-            <div className="experience-time">
-                <Skill technology="Python" measure={3} years="3 years"/>
-                <Skill technology="RPA" measure={3} years="3 years"/>
-                <Skill technology="JavaScript" measure={3} years="3 years"/>
-                <Skill technology="HTML, CSS" measure={3} years="3 years"/>
-                <Skill technology="Node" measure={2} years="2 years"/>
-                <Skill technology="MongoDB" measure={2} years="2 years"/>
-                <Skill technology="React" measure={2} years="2 years"/>
-                <Skill technology="React Native" measure={2} years="2 years"/>
-                <Skill technology="AWS" measure={2} years="2 years"/>
-                <Skill technology="TypeScript" measure={1} years="1 year"/>
-                <Skill technology="Express.js" measure={1} years="1 year"/>
-                <Skill technology="Java" measure={1} years="1 year"/>
-                <Skill technology="PHP" measure={1} years="1 year"/>
-          </div>
-        </div>
-    )
+  return (
+    <div className="experience">
+      <SectionTitle text="Experience" />
+      <p>
+        {
+          "Working as a Software Developer since 2020, I'm familiar with Python Programming, RPA Solutions, Full-Stack Web Development, Mobile, Cloud & DevOps."
+        }
+      </p>
+      <p>
+        {"I'm currently looking for new opportunities to help teams as a "}
+        <em>
+          {
+            "Full-Stack Web Developer, Mobile Developer, DevOps, Python Developer and/or RPA Developer"
+          }
+        </em>
+        .
+      </p>
+      <div className="experience-time">
+        <Skill technology="HTML5 & CSS3" measure={3} years="4 years" />
+        <Skill technology="JavaScript" measure={3} years="4 years" />
+        <Skill technology="Python" measure={3} years="4 years" />
+        <Skill technology="RPA" measure={3} years="3 years" />
+        <Skill technology="MySQL" measure={3} years="3 years" />
+        <Skill technology="Java" measure={2} years="2 years" />
+        <Skill technology="C#" measure={2} years="2 years" />
+        <Skill technology="Node" measure={2} years="2 years" />
+        <Skill technology="MongoDB" measure={2} years="2 years" />
+        <Skill technology="React" measure={2} years="2 years" />
+        <Skill technology="React Native" measure={2} years="2 years" />
+        <Skill technology="AWS" measure={2} years="2 years" />
+        <Skill technology="TypeScript" measure={1} years="1 year" />
+        <Skill technology="Next.js" measure={1} years="1 year" />
+        <Skill technology="Express.js" measure={1} years="1 year" />
+        <Skill technology="PHP" measure={1} years="1 year" />
+      </div>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,31 @@
-import AboutMe from './components/about-me'
-import Experience from './components/experience'
-import Footer from './components/footer'
-import Header from './components/header'
-import Info from './components/info'
+import AboutMe from "./components/about-me";
+import Experience from "./components/experience";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Info from "./components/info";
 
-
-import styles from './styles/page.module.css'
+import styles from "./styles/page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-        <header className={styles.header}>
-          <Header 
-            name='Felipe Roque' 
-            title='Full-Stack Developer & RPA Solutions'
-          />
-        </header>
-        <main className={styles.main}>
-          <AboutMe 
-            description="I started my development journey in 2019, mastering Python and web technologies. In 2020, I launched my career as a Python Developer focused in creating RPA solutions. I've since expanded my skills to Python, Java, JavaScript, TypeScript and others, I'm comfortable working with technologies like React Native, React, Node, Express.js, Python, SQL & NoSQL, and more. Great experience building RPA and I'm seeking for new business opportunities."
-            subDescription="My goal is to innovate through tech, building impactful solutions and create businesses."/>
-          <Experience />
-          <Info />
-        </main>
-        <footer className={styles.footer}>
-          <Footer />
-        </footer>
-      </div>
-  )
+      <header className={styles.header}>
+        <Header
+          name="Felipe Roque"
+          title="Full-Stack Web, Mobile Developer & RPA Python Developer"
+        />
+      </header>
+      <main className={styles.main}>
+        <AboutMe
+          description="I started my software developer journey in 2019, learning basics of web development and Python. I have 2 great diplomas in great schools in São Paulo, Brazil, Computer Technical Diploma at @Senac (2019 - 2020) & Analysis and Systems Developement at @FIAP (2020-2022). I'm working now with Full Stack Web technologies, Mobile Development and Python programming. In 2020, I started my first job as a Python Developer focused in creating and supporting RPA solutions at @BPA. I've expanded my skills to Python, JavaScript, TypeScript, Java, and others, I'm comfortable working with technologies like React, Next.js, React Native, Node, Express.js, Python, Django, Java, Spring, SQL & NoSQL Databases, AWS, Git, and more. Great experience building RPA."
+          subDescription="I'm seeking for new business opportunities. My goal is to innovate through tech, building impactful solutions, improve and create businesses."
+        />
+        <Experience />
+        <Info />
+      </main>
+      <footer className={styles.footer}>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
