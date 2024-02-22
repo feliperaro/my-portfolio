72e77104e01da007368aa9c85a7dacb167f807ee
FehRoque <feliperamosroque@gmail.com>
2024-02-22 11:57:57 -0300

    header using tailwind

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,15 +3,12 @@ type HeaderProps = {
   title: string;
 };
 
-import "./styles.css";
-
 export default function Header(props: HeaderProps) {
   const { name, title } = props;
-
   return (
-    <header>
-      <h1 className="textHeader">{name}</h1>
-      <h2 className="textHeader">{title}</h2>
+    <header className="m-12 text-center w-10/12">
+      <h1 className="font-semibold text-2xl p-1">{name}</h1>
+      <h2 className="font-normal p-1 text-xl">{title}</h2>
     </header>
   );
 }
