b6909486c8a01b766baad6e99e4c049f56f413f8
FehRoque <feliperamosroque@gmail.com>
2024-02-22 13:56:25 -0300

    changed margin

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -6,7 +6,7 @@ type HeaderProps = {
 export default function Header(props: HeaderProps) {
   const { name, title } = props;
   return (
-    <header className="m-12 text-center w-10/12">
+    <header className="m-10 text-center w-10/12">
       <h1 className="font-semibold text-2xl p-1">{name}</h1>
       <h2 className="font-normal p-1 text-xl">{title}</h2>
     </header>
