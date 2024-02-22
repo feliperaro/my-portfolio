a2d8f9dd0f70a6db2d44f3b5ac1c5b0c3c6eb184
FehRoque <feliperamosroque@gmail.com>
2024-02-22 15:43:13 -0300

    body tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import styles from "./styles/page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <body className={styles.container}>
       <Header name="Felipe Ramos Roque" title="Full Stack Software Developer" />
       <main className={styles.main}>
         <AboutMe
@@ -25,6 +25,6 @@ I'm currently looking for new opportunities to help teams as a Full-Stack Web De
       <footer className={styles.footer}>
         <Footer />
       </footer>
-    </div>
+    </body>
   );
 }
