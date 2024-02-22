ab16dba5ea8c6bfdd00300206fcb6e4f8b30a90e
FehRoque <feliperamosroque@gmail.com>
2024-02-22 11:59:17 -0300

    const { text } = props

--- src/app/components/experience/index.tsx
+++ src/app/components/experience/index.tsx
@@ -5,10 +5,11 @@ type ExperienceProps = {
 };
 
 export default function Experience(props: ExperienceProps) {
+  const { text } = props;
   return (
     <div className="flex flex-col gap-3 p-5 w-5/6">
       <SectionTitle text="Experience" />
-      <em className="text-justify">{props.text}</em>
+      <em className="text-justify">{text}</em>
     </div>
   );
 }
