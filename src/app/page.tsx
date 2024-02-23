eee98a330eaac583cf60d9eb461a1300ef3425fa
FehRoque <feliperamosroque@gmail.com>
2024-02-23 10:36:18 -0300

    page.tsx using tailwind + shorter description AboutMe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,13 @@ import Footer from "./components/footer";
 import Header from "./components/header";
 import Info from "./components/info";
 
-import styles from "./styles/page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col w-full">
       <Header name="Felipe Ramos Roque" title="Full Stack Software Developer" />
-      <main className={styles.main}>
+      <main className="flex flex-col gap-1 pt-10 lg:pl-48 lg:pr-48 items-center w-full">
         <AboutMe
-          description="Started my software developer career in 2019, learning Python programming and web development. I have 2 diplomas in great tech schools in Brazil, Computer Technical Diploma at @Senac (2019 - 2020) & Analysis and Systems Development at @FIAP (2020-2022). I'm working with Full Stack Web technologies, Mobile Development and RPA development using Python programming. In 2020, I started my first job as a Python Developer focused in creating and supporting RPA solutions at @BPA. I've expanded my skills to Python, JavaScript, Java, TypeScript and others, I'm comfortable working with technologies like React, React Native, Node, Express.js, Python, Django, Flask, Java and Spring, SQL & NoSQL Databases, AWS, Azure, Git, and others."
+          description="Started my software developer career in 2019, learning Python programming and web development. I'm working with Full Stack Web technologies, Mobile Development and RPA development using Python programming. I've expanded my skills to Python, JavaScript, Java, TypeScript and others, I'm comfortable working with technologies like React Native, Node.js, Python, SQL & NoSQL Databases, AWS, Git, and others."
           subDescription="I'm seeking for new opportunities. My goal is to innovate through tech, building new solutions, improve and create businesses."
         />
         <Experience
