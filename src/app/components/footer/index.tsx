a28807217edaa2a727a374b70a540110ccea5bbb
FehRoque <feliperamosroque@gmail.com>
2024-02-23 10:35:06 -0300

    hover EmailIcon

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-[#272643] flex flex-row gap-6 p-6 justify-center items-center w-full">
       <a
-        className="flex border border-white rounded-xl p-2 pl-5 pr-5 gap-2 text-white hover:text-blue-300"
+        className="flex border border-white rounded-xl p-2 pl-5 pr-5 gap-2 text-white hover:font-bold"
         href="mailto:feliperoque.dev@gmail.com"
       >
         {"Contact me"}
