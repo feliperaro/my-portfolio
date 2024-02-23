7377a1a80bb8ef11a7d1bbb51c1c80021707ae2d
FehRoque <feliperamosroque@gmail.com>
2024-02-23 11:41:51 -0300

    props page to header-nav to semibold current page

--- src/app/components/header-nav/index.tsx
+++ src/app/components/header-nav/index.tsx
@@ -1,15 +1,36 @@
-export default function HeaderNav() {
+export default function HeaderNav(props: { page: string }) {
+  const { page } = props;
   return (
     <nav className="mb-5 p-5 w-full">
       <ul className="flex flex-row justify-center gap-10">
         <li>
-          <a className="hover:font-bold" href="/">
+          <a
+            className={`hover:font-bold ${
+              page === "home" ? "font-semibold" : null
+            }`}
+            href="/"
+          >
             Home
           </a>
         </li>{" "}
         <li>
-          <a className="hover:font-bold" href="/blog">
+          <a
+            className={`hover:font-bold ${
+              page === "blog" ? "font-semibold" : null
+            }`}
+            href="/blog"
+          >
             Blog
+          </a>
+        </li>
+        <li>
+          <a
+            className={`hover:font-bold ${
+              page === "frr-technology" ? "font-semibold" : null
+            }`}
+            href="/frr-technology"
+          >
+            FRR Technology
           </a>
         </li>
       </ul>
